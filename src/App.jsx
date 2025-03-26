6007d8c1e9259c82cedba150995bf07639114b91
DiegoBAmorim <diegobarbosamorim@gmail.com>
2025-03-26 08:48:06 -0300

    refactor: remove unnecessary comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { DrawerProvider } from './hooks/useDrawer';
 import { FeatureLayerProvider } from './hooks/useFeatureLayer';
 import { Home } from './pages/Home';
-// import MapView from '@arcgis/core/views/MapView.js';
 import { SearchProvider } from './hooks/useSearch';
 
 function App() {
